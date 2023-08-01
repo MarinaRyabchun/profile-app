48c28ab67e50c5424bfcb6472812289884ea67e9
Marina Ryabchun <marinarrrr@gmail.com>
2023-08-01 10:37:14 +0300

    Create an empty project

---
+++ profile-app/profile-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  profile-app
+//
+//  Created by Марина Рябчун on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
